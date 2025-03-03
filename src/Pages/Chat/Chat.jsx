4efa0067ce7c3a674d78e037f6c99719fb4c8785
mharoon-dev <haroon.aghani@gmail.com>
2025-03-04 01:24:11 +0500

    change the URL

--- src/Pages/Chat/Chat.jsx
+++ src/Pages/Chat/Chat.jsx
@@ -38,7 +38,7 @@ const Chat = () => {
 
       try {
         // Call the API to get AI response
-        const response = await axios.post("ai-chatbot-server-phi.vercel.app/generate", {
+        const response = await axios.post("https://ai-chatbot-server-phi.vercel.app/generate", {
           prompt: input,
         });
 
